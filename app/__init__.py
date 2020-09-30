10f786e189180e0c299e1e286dd418fc9d6a1b4e
Abhishek Raj <abhishekraj272@gmail.com>
2020-09-30 07:46:21 +0530

    added API Key shuffling

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,33 @@
 from flask import Flask
 import pymongo
-import os
-import sys
+import os, sys
 from requests import get
 import asyncio
 from json import loads
 import threading
 from flask_caching import Cache
 import datetime
+import random
 
-SECRET_KEY = os.urandom(32)
+# For generating random number based on time, used in shuffling APIs.
+random.seed(a=None, version=2)
 
 app = Flask(__name__)
-cache = Cache(app, config={"CACHE_TYPE": "filesystem", 'CACHE_DIR': 'cache-directory', 'CACHE_THRESHOLD': 500})
 
-app.config['SECRET_KEY'] = SECRET_KEY
+# Cache type, directory and threshold is defined here
+cache = Cache(app, config={"CACHE_TYPE": "filesystem", 'CACHE_DIR': 'cache-directory', 'CACHE_THRESHOLD': 50})
 
-MongoURI = os.environ.get('MONGODB_URI', "mongodb+srv://abhishek:Asdfg#1hjkl@cluster0.nkell.mongodb.net/fampay?retryWrites=true&w=majority")
+# Gets the MongoDB URI from env var
+MongoURI = os.environ.get('MONGODB_URI', None)
 
-ytAPIKey = os.environ.get('YTAPIKey', "AIzaSyBlR2Av2WHgV6yDlLLllJXBUL2GdyyCuOE")
+# Set the total number of API Keys are provided for shuffeling
+TotalAPISet = os.environ.get('TotalAPISet', 1)
 
 if MongoURI is None:
-    sys.exit("\n * MongoDB URI not provided.\n")     
+    sys.exit("\n * MongoDB URI not provided.\n")
 
+
+# MongoDB client is initialised
 client = pymongo.MongoClient(MongoURI)
 
 # Selecting database
@@ -31,7 +36,7 @@ db = client.fampay
 # Selecting collection
 collection = db.ytvid
 
-# Custom templat filter to convert time format
+# Custom template filter to convert time format
 @app.template_filter()
 def fmdatetime(value):
     return datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
@@ -63,39 +68,56 @@ def data_insert_to_db(incoming_data):
     except Exception as e:
         print(e)
 
-
+# Async function for getting data from YT API
 async def get_data_from_youtube():
     """
     Function to get data from Youtube useing API.
     """
+    # Generates random number between 1 and total APIs provided 
+    i = random.randint(1, TotalAPISet)
+    
+    # Gets the API Key from env var
+    ytAPIKey = os.environ.get(f'YTAPIKey{i}', None)
+
+    if ytAPIKey is None:
+        sys.exit("\n * Google API Key not provided.\n")
+
+    # Query for getting data from YouTube
+    query = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=date&q=gdp&key={ytAPIKey}"
+
     while True:
-        query = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=date&q=gdp&key=AIzaSyBlR2Av2WHgV6yDlLLllJXBUL2GdyyCuOE"
         try:
             res = get(query)
         except Exception as e:
             print(e)
         
-        if res.status_code != 200:
-            print(res.content)
-            if res.status_code == 403:
-                query = 
+        # Checks if data is fetched or not
+        if res.status_code == 200:
 
-        res = res.content.decode("utf-8")
+            res = res.content.decode("utf-8")
+            print('Received data from YT')
+            incoming_data = loads(res)
+            data_insert_to_db(incoming_data["items"])
         
-        print('Received data from YT')
+        else:
+            # Please see line 26-30 for details
+            i = random.randint(1, TotalAPISet)
+            ytAPIKey = os.environ.get(f'YTAPIKey{i}', None)
+            query = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=date&q=gdp&key={ytAPIKey}"
 
-        incoming_data = loads(res)
-        data_insert_to_db(incoming_data["items"])
         await asyncio.sleep(60)
 
-# def loop_in_thread(loop):
-#     asyncio.set_event_loop(loop)
-#     loop.run_until_complete(get_data_from_youtube())
+# Function for initialising Async Loop
+def loop_in_thread(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(get_data_from_youtube())
 
-# loop = asyncio.get_event_loop()
-# t = threading.Thread(target=loop_in_thread, args=(loop,))
-# t.start()
+# Puts the get request in background 
+loop = asyncio.get_event_loop()
+t = threading.Thread(target=loop_in_thread, args=(loop,))
+t.start()
 
+# imports views from views.py
 from app import views
 
 if __name__ == "__main__":
