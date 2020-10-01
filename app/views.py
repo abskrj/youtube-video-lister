d1b8088a9ced7be017749dbc894d58e6b5480efa
hkmekala <haribabbu.krishna@gmail.com>
2020-10-01 11:45:34 +0530

    Changed the Health Endpoint and reformatted the file to PEP8
    - Added Setup Check
    - Added Database check
    - Added Application check
    - Reformatted file to PEP8.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from app import app, collection, cache, client
-from flask import render_template, request, redirect, url_for
-import re
-from json import loads
+from flask import render_template, request, redirect, url_for, jsonify
+import os
+import random
+import pymongo
+
 
 @app.route("/")
 def index():
     return redirect(url_for('dashboard'))
+
 
 @app.route("/dashboard")
 @cache.cached(timeout=60, query_string=True)
@@ -14,21 +17,46 @@ def dashboard():
     maxResults = request.args.get('maxResults', 10)
     sortBy = request.args.get('sortBy', 'publishedAt')
     start = request.args.get('start', 0)
-    
+
     data = collection.find().skip(int(start)).sort([(sortBy, int(invertList))]).limit(int(maxResults))
-    
+
     return render_template("index.html", data=data)
+
 
 @app.route("/reqbin-verify.txt")
 def reqbin():
     return "", 200
 
+
 @app.route("/status")
-def status()
-    return "Up and Running ;)", 200
+def status():
+    database = False
+    application = True
+    setup = False
+    MongoURI = os.environ.get('MONGODB_URI', None)
+    TotalAPISet = int(os.environ.get('TotalAPISet', 1))
+    i = str(random.randint(1, TotalAPISet))
+    ytAPIKey = os.environ.get(f'YTAPIKey{i}', None)
+
+    if MongoURI is not None and ytAPIKey is not None and ytAPIKey is not None:
+        setup = True
+    try:
+        client = pymongo.MongoClient(MongoURI)
+        if client.server_info() is not None:
+            database = True
+    except:
+        database = False
+
+    return jsonify(
+        status=True,
+        database=database,
+        application=application,
+        setup=setup
+    )
+
 
 @app.route("database-info")
-def database_info()
+def database_info():
     try:
         client.server_info()
     except Exception as e:
